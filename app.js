6b1c92fb88e4f26e161be3458be9fb470894cad1
Nourin <nourinawad@gmail.com>
2026-01-18 21:08:54 +0200

    setup swagger

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
@@ -12,6 +14,25 @@ import cookieParser from 'cookie-parser';
 import arcjetMiddleware from './middleware/arcjet.middleware.js';
 
 const app = express();
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerOptions = {
+    customCss: `
+        .swagger-ui .topbar { display: none }
+        .swagger-ui .info { margin: 50px 0 }
+        .swagger-ui .info .title { color: #2c3e50 }
+    `,
+    customSiteTitle: "Subscription Tracker API",
+    customfavIcon: "/favicon.ico",
+    swaggerOptions: {
+        persistAuthorization: true, // Keep auth token after refresh
+        displayRequestDuration: true,
+        filter: true, // Enable filtering
+        tryItOutEnabled: true
+    }
+};
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
