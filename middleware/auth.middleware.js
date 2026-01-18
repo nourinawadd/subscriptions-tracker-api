152bfd8ebe58b25ba349ac0d96fc95122a42d543
Nourin <nourinawad@gmail.com>
2026-01-18 13:37:46 +0200

    add authorization

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config/env.js';
+import User from '../models/user.model.js';
+
+const authorize = async (req, res, next) => {
+    try {
+        let token;
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if (!token) {
+            const error = new Error('No token provided, authorization denied');
+            error.statusCode = 401;
+            throw error;
+        }
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        const user = await User.findById(decoded.userId).select('-password');
+        if(!user) return res.status(401).json({ success: false, message: 'Unauthorized' });
+
+        req.user = user;
+        next();
+    }
+    catch (error) {
+        res.status(401).json({ success: false, message: 'Unauthorized', error: error.message });
+    }
+}
+
+export default authorize;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getUsers, getUser } from '../controllers/user.controller.js';
+import authorize from '../middleware/auth.middleware.js';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => {
     res.send( {title: 'CREATE new user'});
